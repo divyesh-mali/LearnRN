df1c7b763c98d643574962148f5d619eedb4e44b
Divyesh Mali <medivyeshmali@gmail.com>
2024-08-03 21:38:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ import App from './App';
 import {name as appName} from './app.json';
 import buttonInfo from './Buttons/buttonInfo';
 import UseStateDemo from './useState/useStateDemo';
-import externalStyling from './ExternalStyling/externalStyling';
+import externalStyling from './UIandStyling/externalStyling';
+import textInput from './TextInput/textInput';
+import formInput from './TextInput/formInput';
+import flatList from './TextInput/flatList';
+import mapFunction from './TextInput/mapFunction';
+import gridView from './UIandStyling/gridView';
 
-AppRegistry.registerComponent(appName, () => externalStyling);
+AppRegistry.registerComponent(appName, () => gridView);
