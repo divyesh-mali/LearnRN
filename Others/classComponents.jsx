618b9b8c5d56589578cdeaf6b695e25bcad87205
Divyesh Mali <medivyeshmali@gmail.com>
2024-08-03 23:29:38 +0530

    Class Components

---
+++ Others/classComponents.jsx
@@ -0,0 +1,33 @@
+import { Button, Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+class classComponents extends Component {
+
+    // To handle events in class components, we need to define a function without variable name like given below
+    handlePress = () => {
+        console.log('Button pressed');
+        
+    }
+
+    render() {
+        return (
+            <View>
+                <Child/>
+                <Text>Hey Class Components</Text>
+                <Button title="Press me" onPress={this.handlePress} />{/* To access that function which is created without variable name, use 'this' keyword */}
+            </View>
+        );
+    }
+}
+
+class Child extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Child Component</Text>
+            </View>
+        );
+    }
+}
+
+export default classComponents
